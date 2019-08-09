1f066bb02da94e42118f52d28451f84104c11636
毕殿卿 <bdq123@outlook.com>
2019-08-09 10:18:29 +0800

    添加项目文件。

---
+++ AspNetCoreUseQueueBackgroundWorkItem/Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace AspNetCoreUseQueueBackgroundWorkItem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public ValuesController(IBackgroundTaskQueue queue)
+        {
+            Queue = queue;
+        }
+
+        public IBackgroundTaskQueue Queue { get; }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            Queue.QueueBackgroundWorkItem(async (token) =>
+            {
+                await Console.Out.WriteLineAsync("Hello World!");
+            });
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
